419e7585b7421649a705f469a41bb926522cec7f
kostas4419 <47432222+kostas4419@users.noreply.github.com>
2024-12-18 00:51:34 +0300

    Update index.js

--- src/http/index.js
+++ src/http/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const API_URL = 'https://test-crmleads.ru:5000/api'
+export const API_URL = 'https://test-crmleads.ru/api'
 
 const $api = axios.create({
     withCredentials:true,
